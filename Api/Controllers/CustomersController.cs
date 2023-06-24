1f6fae257473f78f1d2ceefd0adf9149a85eeaa3
danielNG <dacnguyen9101@gmail.com>
2023-06-24 16:39:52 +0700

    feat: fix logic update user

--- Api/Controllers/CustomersController.cs
+++ Api/Controllers/CustomersController.cs
@@ -10,7 +10,7 @@ using Repository;
 
 namespace Api.Controllers;
 
-[Authorize(Roles = PolicyName.ADMIN)]
+[Authorize]
 [Route("api/v1/customers")]
 public class CustomersController : BaseController {
     private readonly IRepository<AspNetUser> _customerRepository;
@@ -30,12 +30,14 @@ public class CustomersController : BaseController {
         _customerRepository = customerRepository;
     }
 
+    [Authorize(Roles = PolicyName.ADMIN)]
     [HttpGet]
     public async Task<IActionResult> GetCustomers() {
         return Ok(await _customerRepository.ToListAsync());
     }
 
 
+    [Authorize(Roles = PolicyName.ADMIN)]
     [HttpPost]
     public async Task<IActionResult> CreateCustomer([FromBody] RegisterRequest req) {
         await ValidateRegisterFields(req);
@@ -71,31 +73,44 @@ public class CustomersController : BaseController {
         }
     }
 
+
+    [Authorize(Roles = PolicyName.ADMIN)]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetCustomer(int id) {
         var target = await _customerRepository.FoundOrThrow(c => c.Id == id, new NotFoundException());
         return Ok(target);
     }
 
-    [Authorize(Roles = $"{PolicyName.ADMIN},{PolicyName.CUSTOMER}")]
     [HttpGet("profile")]
     public async Task<IActionResult> GetProfile() {
         var target = await _customerRepository.FirstOrDefaultAsync(c => c.Id == CurrentUserID);
         return Ok(target);
     }
 
-    [Authorize(Roles = $"{PolicyName.ADMIN},{PolicyName.CUSTOMER}")]
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCustomer(int id, [FromBody] UpdateCustomerRequest req) {
-        if (!IsAdmin) {
-            id = CurrentUserID;
-        }
-        var target = await _customerRepository.FoundOrThrow(c => c.Id == id, new NotFoundException());
-        var entity = Mapper.Map(req, target);
-        await _customerRepository.UpdateAsync(entity);
+    [HttpPut("profile")]
+    public async Task<IActionResult> UpdateCustomer([FromBody] UpdateCustomerRequest req) {
+        var target = await _customerRepository.FoundOrThrow(c => c.Id == CurrentUserID, new NotFoundException());
+        await UpdateUser(req, target);
         return StatusCode(StatusCodes.Status204NoContent);
     }
 
+    [Authorize(Roles = PolicyName.ADMIN)]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateCustomer(int id, [FromBody] UpdateCustomerRequest req) {
+        var target = await _customerRepository.FoundOrThrow(c => c.Id == id, new NotFoundException());
+        await UpdateUser(req, target);
+        return StatusCode(StatusCodes.Status204NoContent);
+    }
+
+    private async Task UpdateUser(UpdateCustomerRequest req, AspNetUser user) {
+        var entity = Mapper.Map(req, user);
+        if (!string.IsNullOrEmpty(req.Password)) {
+            entity.PasswordHash = _userManager.PasswordHasher.HashPassword(entity, req.Password);
+        }
+        await _userManager.UpdateAsync(entity);
+    }
+
+    [Authorize(Roles = PolicyName.ADMIN)]
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCustomer(int id) {
         var target = await _userManager.FindByIdAsync(id.ToString());
